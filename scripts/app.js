482c26bd595843e7e5d0160355c7398a46570729
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2021-03-17 16:16:36 -0500

    adds CSS and JS for animations

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,29 @@
-console.log('Hey there');
+let header = document.querySelector('.header');
+let site = document.querySelector('.site');
+let cta = document.querySelector('.cta');
+
+let imageOverlay = document.querySelector('.image-container').children[0];
+let mainImage = document.querySelector('.image-container').children[1];
+
+let links = Array.from(document.querySelectorAll('.link'));
+
+function animate() {
+  setTimeout(() => {
+    header.classList.add('active');
+    imageOverlay.style.animation = 'slide 1s forwards';
+    mainImage.style.animation = 'slide-perm 1s forwards';
+  }, 2000);
+
+  setTimeout(() => {
+    site.classList.add('active');
+    cta.classList.add('active');
+  }, 3000);
+
+  links.forEach((link, index) => {
+    setTimeout(() => {
+      link.classList.add('active');
+    }, (index * 200) + 2500);
+  })
+}
+
+animate();
